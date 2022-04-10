7518a4c8a831eb4a47f52cd7945f65ded9aa0eda
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-04-10 16:19:53 +0530

    assigned variables

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+const p_div = document.querySelector(".password");
+const copy_btn = document.querySelector(".copy_btn");
+const upper = document.querySelector("#upper");
+const lower = document.querySelector("#lower");
+const num = document.querySelector("#num");
+const symbol = document.querySelector("#symbol");
+const slider = document.querySelector("#slider");
+const len_box = document.querySelector("#len_value");
+const run_btn = document.querySelector("#btn");
+
