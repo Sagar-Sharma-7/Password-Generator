da2207cee5c9169cd500908b7e05b2fede0f39db
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-04-10 17:10:14 +0530

    arr of selected char

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ const upper = document.querySelector("#upper");
 const lower = document.querySelector("#lower");
 const num = document.querySelector("#num");
 const symbol = document.querySelector("#symbol");
+const checkbox = document.querySelectorAll(".checkbox");
 const slider = document.querySelector("#slider");
 const len_box = document.querySelector("#len_value");
 const run_btn = document.querySelector("#btn");
@@ -17,4 +18,14 @@ const n_char = [...'0123456789']; // numbers array
 const s_char = [...'!@#$&*?_-']; // symbols array
 
 
-
+run_btn.addEventListener("click", ()=> {
+    // r1 = Math.floor(Math.random() * 4); 
+    let arr = []
+    checkbox.forEach(e => {
+        if(e.checked){
+            arr.push(e.name);
+        }
+    });
+    l = arr.length;
+    console.log(l)
+});
