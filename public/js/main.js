ddaa416e1fff57d8849e8047aed6d00bb3cd6c3a
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-04-11 12:17:08 +0530

    added some imp comments

--- public/js/main.js
+++ public/js/main.js
@@ -11,21 +11,30 @@ const len_box = document.querySelector("#len_value");
 const run_btn = document.querySelector("#btn");
 
 
+// change value of password length as we move the slider.
+len_box.innerHTML = slider.value;
+slider.oninput = function(){
+    len_box.innerHTML = this.value;
+};
 
+
+// creating arr of each type of char
 const u_char = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']; // uppercase letter array
 const l_char = [...'abcdefghijklmnopqyrstuvwxyz']; // lowercase letter array
 const n_char = [...'0123456789']; // numbers array
 const s_char = [...'!@#$&*?_-']; // symbols array
 
 
+// operations runned after clicking "generate password" btn
 run_btn.addEventListener("click", ()=> {
-    // r1 = Math.floor(Math.random() * 4); 
+    // 1. Get password length
+    console.log(slider.value);
     let arr = []
     checkbox.forEach(e => {
         if(e.checked){
             arr.push(e.name);
         }
     });
-    l = arr.length;
-    console.log(l)
+    // array with selected char type is ready.
+
 });
